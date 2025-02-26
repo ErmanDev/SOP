a3bb797b5cb6206fd05efaf51e9274885c491fe0
narra <ermanfaminiano020@gmail.com>
2025-02-26 17:14:25 +0800

    feat: :sparkles: update code

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,7 +7,7 @@ router.get("/:id", userController.getUserById);
 router.get('/countUsers', userController.countUsers);
 router.get('/countTechnicians', userController.countTechnicians);
 router.get('/countManagers', userController.countManagers);
-router.post("/login", userController.loginUser); 
+router.put("/:id", userController.updateUser);
 router.delete("/:id", userController.deleteUser); 
 
 
