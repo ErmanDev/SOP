c0f652a1d85c064cab63af773724c4b970f1c75b
Trppypata <leahpardet19.tp@gmail.com>
2025-02-28 11:55:38 +0800

    sadjlakdjlasd

--- frontend/src/components/login-form.tsx
+++ frontend/src/components/login-form.tsx
@@ -18,7 +18,7 @@ export function LoginForm({
   const [password, setPassword] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
-  const render_url = process.env.render_url;
+  // const render_url = process.env.render_url;
 
   const handleLogin = async () => {
     setIsLoading(true);
