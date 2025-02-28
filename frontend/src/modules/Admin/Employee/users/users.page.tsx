83f18cf4b3489fa598cfcc1c4cce5c732c90196b
narra <ermanfaminiano020@gmail.com>
2025-02-28 14:12:56 +0800

    dasdasd

--- frontend/src/modules/Admin/Employee/users/users.page.tsx
+++ frontend/src/modules/Admin/Employee/users/users.page.tsx
@@ -1,10 +1,10 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/sr-tabs";
-import { Link, Outlet, useLocation } from "react-router-dom";
-import UsersList from "./user-list";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/sr-tabs';
+import { Link, Outlet, useLocation } from 'react-router-dom';
+import UsersList from './user-list';
 
 const UserPage = () => {
   const location = useLocation();
-  const currentTab = location.pathname.split("/").pop(); // Get current tab based on the route
+  const currentTab = location.pathname.split('/').pop(); // Get current tab based on the route
 
   return (
     <Tabs defaultValue="users-list">
@@ -13,19 +13,24 @@ const UserPage = () => {
           <Link to={'users-list'}>
             <TabsTrigger value="users-list">All</TabsTrigger>
           </Link>
-            
-          <Link to={"archive"}>
+
+          <Link to={'archive'}>
             <TabsTrigger value="aboutpage">Archive</TabsTrigger>
           </Link>
         </TabsList>
       </div>
 
-        <TabsContent value={currentTab as string === "users" ? "users-list" : currentTab as string}>
-          {currentTab === 'users' ? <UsersList /> : <Outlet />}
-
-        </TabsContent>
+      <TabsContent
+        value={
+          (currentTab as string) === 'users'
+            ? 'users-list'
+            : (currentTab as string)
+        }
+      >
+        {currentTab === 'users' ? <UsersList /> : <Outlet />}
+      </TabsContent>
     </Tabs>
-  )
-}
+  );
+};
 
-export default UserPage
+export default UserPage;
