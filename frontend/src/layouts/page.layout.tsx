38e106fc3f94bd68861bd9a6f2ef6844139e5489
narra <ermanfaminiano020@gmail.com>
2025-03-05 09:19:29 +0800

    feat: :sparkles: no scrollbar in login

--- frontend/src/layouts/page.layout.tsx
+++ frontend/src/layouts/page.layout.tsx
@@ -1,11 +1,12 @@
-import { Outlet } from "react-router-dom"
+import { Outlet } from 'react-router-dom';
 const PageLayout = () => {
   return (
     <div className="min-h-screen bg-white">
-    <main className="max-w-6xl mx-auto px-4 py-16">
-      <Outlet />
-    </main>
-  </div>
-)}
+      <main className="max-w-6xl mx-auto px-4 ">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
 
-export default PageLayout
+export default PageLayout;
